51030d6b41eb3529ac3f59ed84e43ee13843ab22
julichile <julia.berning@thenotcompany.com>
2024-09-22 20:19:08 -0300

    feat: user registration and login

--- feedback/views.py
+++ feedback/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+    return render(request, 'feedback/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'feedback/register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'feedback/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('login') 
