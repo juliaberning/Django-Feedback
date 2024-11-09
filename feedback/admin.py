9e971a75fcfa3aee338e7b001504da71554d7bb2
Julia <julia.berning@thenotcompany.com>
2024-11-09 17:42:08 -0300

    feat: Admin models for Feedback Process and Reviews

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile, FeedbackProcess, Review
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'get_first_name', 'get_last_name', 'get_email', 'department_display', 'manager_display')
+    list_display = ('user', 'get_first_name', 'get_last_name', 'get_email', 'department', 'manager')
 
     def get_first_name(self, obj):
         return obj.user.first_name
@@ -16,10 +16,52 @@ class UserProfileAdmin(admin.ModelAdmin):
         return obj.user.email
     get_email.short_description = 'Email' 
 
-    def department_display(self, obj):
+    def department(self, obj):
         return dict(UserProfile.DEPARTMENT_CHOICES).get(obj.department)
-    department_display.short_description = 'Department' 
-    def manager_display(self, obj):
+    department.short_description = 'Department' 
+    def manager(self, obj):
         return obj.manager.user.username if obj.manager else None
-    manager_display.short_description = 'Manager'
+    manager.short_description = 'Manager'
 admin.site.register(UserProfile, UserProfileAdmin)
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ( 'department','reviewee','feedback_process_id', 'reviewer', 'review_id', 'review_text')
+     
+     
+    def department(self, obj):
+        return dict(UserProfile.DEPARTMENT_CHOICES).get(obj.feedback_process.reviewee.department)
+    department.short_description = 'Department'
+
+    def reviewee(self, obj):
+        return obj.feedback_process.reviewee.user.username
+    reviewee.short_description = 'Reviewee'
+
+    
+    def feedback_process_id(self, obj):
+        return obj.feedback_process.id
+    feedback_process_id.short_description = 'Feedback Process ID'
+
+
+    def review_id(self, obj):
+        return obj.id
+    review_id.short_description = 'Review ID'
+
+    def reviewer(self, obj):
+        return obj.reviewer.user.username if obj.reviewer else 'No reviewer'
+    reviewer.short_description = 'Reviewer'
+
+    
+    def review_text(self, obj):
+        return obj.review_text
+    review_text.short_description = 'Review Text'
+admin.site.register(Review, ReviewAdmin)
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 0
+class FeedbackProcessAdmin(admin.ModelAdmin):
+    list_display = ('reviewee', 'manager')
+    search_fields = ('reviewee__user__username', 'manager__user__username')
+    inlines = [ReviewInline]
+admin.site.register(FeedbackProcess, FeedbackProcessAdmin)
